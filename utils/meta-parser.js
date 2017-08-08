c47193138f1e3a21e46abdfa65b164eb177478a1
Lenin Hasda <leninhasda@gmail.com>
2017-08-08 23:27:26 +0600

    meta parser added

---
+++ utils/meta-parser.js
@@ -0,0 +1,29 @@
+const YAML = require('js-yaml');
+
+let meta = {};
+
+module.exports = MetaParser;
+
+function MetaParser(md) {
+    let metaData = [];
+    let data = [];
+    let line = 0;
+
+    if ( ! md[line].match(/^---$/) ) {
+        data = md;
+    }
+    else {
+        for (line = 1; line < md.length; line++) {
+            if(md[line].match(/^---$/)) {
+                break;
+            }
+            metaData.push(md[line]);
+        }
+        data = md.slice(line+1);
+    }
+
+    return {
+        meta: YAML.safeLoad(metaData.join('\n')) || {},
+        content: data.join('\n')
+    };
+}
